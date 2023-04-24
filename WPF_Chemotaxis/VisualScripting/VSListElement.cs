1b75510d3bbd829e57d24768122f2e653200356f
Luke Tweedy <luke.tweedy@gmail.com>
2023-04-24 16:19:12 +0100

    Stating work

---
+++ WPF_Chemotaxis/VisualScripting/VSListElement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_Chemotaxis.UX;
+
+namespace WPF_Chemotaxis.VisualScripting
+{
+    internal class VSListElement
+    {
+        private Type linkedType;
+
+
+
+    }
+}
---
+++ WPF_Chemotaxis/VisualScripting/VSType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_Chemotaxis.UX;
+
+namespace WPF_Chemotaxis.VisualScripting
+{
+    public interface VSFriendlyLinkable : ILinkable
+    {
+        public static string symbolResource { get; }
+        public static double symbolSize { get;  }
+    }
+}
