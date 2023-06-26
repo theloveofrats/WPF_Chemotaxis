b12e10451a6649b89e3207f9fb582b28b1c2f4da
Luke Tweedy at BICR <luke.tweedy@gmail.com>
2023-06-26 17:17:29 +0100

    Starting a change to "Dockable" and "LineConnector" as separate (but not mutually exclusive) relation attributes.

---
+++ WPF_Chemotaxis/VisualScripting/DockableAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Chemotaxis.VisualScripting
+{
+    //Annotation for creating dockable VS elements. Assumes this is child if child is not found.
+    internal class DockableAttribute : Attribute
+    {
+        public double dockDistance = 25;
+        public string parentPropertyName;
+        public string childPropertyName = "";
+    }
+}
---
+++ WPF_Chemotaxis/VisualScripting/LineConnectorAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Chemotaxis.VisualScripting
+{
+    internal class LineConnectorAttribute
+    {
+        public string parentPropertyName;
+    }
+}
